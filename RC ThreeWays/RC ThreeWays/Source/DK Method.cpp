78aeab98212ad8485d7b958c908c16b94188961b
tgarvs <tgarvey12@gmail.com>
2025-10-22 09:42:10 +0100

    included DK Method

--- RC ThreeWays/RC ThreeWays/Source/DK Method.cpp
+++ RC ThreeWays/RC ThreeWays/Source/DK Method.cpp
@@ -9,3 +9,30 @@
 */
 
 #include "DK Method.h"
+
+
+float DKMethod::process_sample(float n){
+    float Vout = (n * (R/(R + Z))) + (X * (R/(R + Z)));
+    X = ((2/Z)*Vout) - X;
+    return Vout;
+}
+
+
+void DKMethod::prepare (float newFs){
+    if(newFs != fs){
+        fs = newFs;
+        update_coefficients();
+    }
+}
+
+
+void DKMethod::setKnobs(float cutoff){
+    if(cutoff != C){
+        C = cutoff;
+        update_coefficients();
+    }
+}
+
+void DKMethod::update_coefficients(){
+    float Z = 1/(2* fs * C);
+}
