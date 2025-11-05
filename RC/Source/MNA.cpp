958e6471a29d9632e6d88d0a313f677c32df3fb4
tgarvs <tgarvey12@gmail.com>
2025-11-05 18:27:42 +0000

    included eigen and wrote MNA (NOT TESTED)

--- RC/Source/MNA.cpp
+++ RC/Source/MNA.cpp
@@ -1,3 +1,50 @@
 
 
 #include "MNA.h"
+
+float MNA::process_sample(float n){
+    b(1, 2) = n;
+    x = (((H - G)*x) + b + b_delay)/A_inv;
+    b_delay = b;
+    
+    return x(1, 2);
+}
+
+
+void MNA::prepare(float sr){
+    if(sr != samp_rate){
+        samp_rate = sr;
+        update_coefficients();
+    }
+}
+
+
+void MNA::set_knobs(float capacitor, float resistor){
+    if(capacitor != Cap){
+        Cap = capacitor;
+        update_coefficients();
+    }
+    
+    if(resistor != Res){
+        Res = resistor;
+        update_coefficients();
+    }
+}
+
+
+void MNA::update_coefficients(){
+    //lol this is def not the best way to do this and im pretty sure its not memory safe
+    G = Eigen::Matrix<float, 3, 3> {{1/Res, -1/Res, 1},
+        {-1/Res, 1/Res, 0},
+        {1, 0, 0}};
+    
+    C = Eigen::Matrix<float, 3, 3> {{0, 0, 0},
+        {0, Cap, 0},
+        {0, 0, 0}};
+    
+    H = (2*C)/T;
+    A = G + H;
+    A_inv = A.inverse();
+    T = 1/samp_rate;
+
+}
--- RC/Source/MNA.h
+++ RC/Source/MNA.h
@@ -1,6 +1,7 @@
 
 
 #pragma once
+#include <Eigen/Dense>
 
 
 class MNA {
@@ -10,7 +11,7 @@ public:
     
     float process_sample(float n);
     void prepare(float sr);
-    void set_knobs(float cutoff);
+    void set_knobs(float capacitor, float resistor);
     
 private:
     
@@ -20,19 +21,39 @@ private:
     float Res = 10000.f;
     float Cap = 10000.f;
     
-    //Matrices
-    
-    //G
-    //b
-    //x
-    //C
-    //A
-    //A_inverse
-    
-    //Extra condensed things
+    //Extra things
     float samp_rate = 44100;
     float T = 1/samp_rate;
-    //float H = (2*C)/T;
+    float Vs = 0; //this will be our input
+    float n_delay = 0;
     
+    //Matrices
+    Eigen::Matrix<float, 3, 3> G {
+        {1/Res, -1/Res, 1},
+        {-1/Res, 1/Res, 0},
+        {1, 0, 0}};
     
+    Eigen::Matrix<float, 1, 3> b {
+        {0},
+        {0},
+        {0}}; //This is Vs, our initial input
+    
+    Eigen::Matrix<float, 1, 3> b_delay {
+        {0},
+        {0},
+        {0}}; //This is Vs, our initial input
+    
+    Eigen::Matrix<float, 1, 3> x { //the unknown vector...Vb (or the second on in the vector) will be our Vout
+        {0}, //start with all zeroes
+        {0},
+        {0}};
+    
+    Eigen::Matrix<float, 3, 3> C {
+        {0, 0, 0},
+        {0, Cap, 0},
+        {0, 0, 0}};
+    
+    Eigen::Matrix<float, 3, 3> H = (2*C)/T;
+    Eigen::Matrix<float, 3, 3> A = G + H;
+    Eigen::Matrix<float, 3, 3> A_inv = A.inverse();
 };
